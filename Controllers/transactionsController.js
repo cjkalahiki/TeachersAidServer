ede93b2de527c16c37fb068b50d65bbd90e45b32
Connor Kalahiki <connorkalahiki@gmail.com>
2021-04-04 22:34:41 -0700

    fully functional and implemented

--- Controllers/transactionsController.js
+++ Controllers/transactionsController.js
@@ -25,12 +25,109 @@ router.post('/transaction', validateJWT, async (req, res) => {
             error: `Failed to make transaction: ${err}`
         })
     }
-    
 });
 
 // get all transactions by a specific user
-router.get('/', async (req, res) => {
-    
+router.get('/', validateJWT, async (req, res) => {
+    const id = req.user.id;
+
+    try {
+        const donorTransactions = await models.TransactionsModel.findAll({
+            where: {
+                userId: id
+            }
+        })
+        res.status(200).json(donorTransactions);
+    } catch (err) {
+        res.status(500).json({error: err})
+    }
+});
+
+//get transaction by id; for diving deeper into the info of a transaction, displaying one transaction 
+router.get('/:id', async (req,res) => {
+    const transactionId = req.params.id;
+
+    try {
+        const query = await models.TransactionsModel.findOne({
+            where: {
+                id: transactionId
+            }
+        });
+        if (!query){
+            res.status(400).json({
+                message: 'Transaction does not exist'
+            })
+        } else {
+            res.status(200).json({
+                message: 'Transaction successfully retrieved.',
+                transaction: query
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+});
+
+//update by transaction id
+router.put('/:id', validateJWT, async (req, res) => {
+    const {amount} = req.body.transaction;
+    const transactionId = req.params.id;
+    const donorId = req.user.id;
+
+    const query = {
+        where: {
+            id: transactionId,
+            userId: donorId
+        }
+    }
+
+    const updatedTransaction = {
+        amount: amount
+    }
+
+    try {
+        await models.TransactionsModel.update(updatedTransaction, query);
+        res.status(200).json({
+            message: 'Transaction successfully updated'
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+});
+
+//delete transaction
+router.delete('/:id', validateJWT, async (req, res) => {
+    const donorId = req.user.id;
+    const transactionId = req.params.id;
+
+    try {
+        const query = {
+            where: {
+                id: transactionId,
+                userId: donorId
+            }
+        };
+
+        const deleteQuery = await models.TransactionsModel.destroy(query);
+
+        if (deleteQuery) {
+            res.status(200).json({
+                message: 'Transaction removed.'
+            })
+        } else {
+            res.status(400).json({
+                message: 'Error, transaction does not exist.'
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
 });
 
 module.exports = router;
