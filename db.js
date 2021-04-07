5e573582d05fefda0f97958fed5a035816606a34
Connor Kalahiki <connorkalahiki@gmail.com>
2021-04-07 02:09:38 -0700

    updated db

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const {Sequelize} = require('sequelize');
 
 const seqeulize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: process.env.DATABASE_DIALECT,
+    ssl: process.env.ENVIRONMENT === 'production'
 })
 
 module.exports = seqeulize;
